661de720adc41df348e275b55d39b286fddf85af
MkMula <maho.mamulashvili@gmail.com>
2022-03-31 10:02:12 -0400

    Add simple mathematical operation
    Print result of the operation.

--- arithmetic.py
+++ arithmetic.py
@@ -1,1 +1,17 @@
 # write your code here
+usr_inp = input().split()
+
+if '+' in usr_inp:
+    x = int(usr_inp[0])
+    y = int(usr_inp[2])
+    print(x + y)
+elif '*' in usr_inp:
+    x = int(usr_inp[0])
+    y = int(usr_inp[2])
+    print(x * y)
+
+elif '-' in usr_inp:
+    x = int(usr_inp[0])
+    y = int(usr_inp[2])
+    print(x - y)
+
