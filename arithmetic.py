978c86917625c6d62702e045b7405a50b097c487
Mk Mula <mkmula@mke.lan>
2022-04-05 16:16:48 -0400

    Add more tasks
    Generate 5 tasks. If the naswer contains typo (letters or empty
    string) ask to re-enter the answer. Repeat until the answer
    is in the correct format.
    
    After five tasks, output 'Your mark is n/5.' where n is the number
    of correct answers.

--- arithmetic.py
+++ arithmetic.py
@@ -1,16 +1,40 @@
 from random import randint, choice
-random_x, random_y = randint(2, 9), randint(2, 9)
-operator = choice(['+', '-', '*'])
-
-print(random_x, operator, random_y)
+from re import search
 
 
-if operator == '+':
-    result = random_x + random_y
-elif operator == '-':
-    result = random_x - random_y
-elif operator == '*':
-    result = random_x * random_y
+class TypoError(Exception):
+    def __str__(self):
+        return 'Incorrect format'
 
 
-print('Right!' if result == int(input()) else 'Wrong!')
+def check_typo(answer):
+    if not answer or search('[A-z]', answer):
+        raise TypoError
+    else:
+        return int(answer)
+
+
+counter, correct = 0, 0
+while counter < 5:
+    random_x, random_y = randint(2, 9), randint(2, 9)
+    operator = choice(['+', '-', '*'])
+    task = f"{random_x}{operator}{random_y}"
+    print(task)
+
+    result = eval(task)
+    while True:
+        try:
+            usr_inp = check_typo(input())
+        except TypoError as err:
+            print(err)
+        else:
+            if result == usr_inp:
+                correct += 1
+                print('Right!')
+            else:
+                print('Wrong!')
+
+            counter += 1
+            break
+
+print(f'Your mark is {correct}/5')
